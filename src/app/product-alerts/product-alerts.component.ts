4e515ab285c474e194c02564f00a9405254cc6b2
WalterSWoodward <walter.sekope.woodward@gmail.com>
2019-09-19 20:28:23 -0400

    imports Output and EventEmitter into product-alerts child component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// Have to first import Input in order to use @input to bring in product object values from products.ts
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -7,6 +9,10 @@ import { Input } from '@angular/core';
   styleUrls: ['./product-alerts.component.scss']
 })
 export class ProductAlertsComponent implements OnInit {
+  // Need this in order to be able to access products.ts values in products-alerts.component.html
+  // @Input indicates that the property "product" will be passed in from the components PARENT e.g. product-list
+  // SO...this means that you will only ever see @Input used for a property that has ALREADY been brought into the
+  // view on a higher level e.g. product, which is in the product-list component view via *ngFor="let product of products"
   @Input() product;
   constructor() { }
 
