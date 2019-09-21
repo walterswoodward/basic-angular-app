d7b849165f680438214769131d0a8154b0206f50
WalterSWoodward <walter.sekope.woodward@gmail.com>
2019-09-21 09:56:49 -0400

    Use the cart service Defines addToCart method in product-details component, which adds the currently viewed product to the cart via cartService's #addToCart method Then USE that addToCart method via a click event on a Buy Now button

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
+import { MethodCall } from '@angular/compiler';
 
 
 @Component({
@@ -11,9 +13,20 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product;
+  // Note that there are TWO different methods here. This here is
+  // a method AND this.cartService.addToCart is ALSO a method.
+  // Defining this addToCart here MUST somehow enable you to call
+  // it somewhere else within this scope - e.g. the Buy Button
+  // inside PDC's view.
+  addToCart(bubbles) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(bubbles);
+    console.log('What is this?', this);
+  }
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService,
   ) { }
 
   ngOnInit() {
